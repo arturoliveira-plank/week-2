9eb36c5a46c46be968a3a630d30eacc7677ab999
Artur H. de Oliveira <artur.oliveira@joinplank.com>
2025-03-18 14:30:33 -0300

    Add 'to-parent-graph' functionality to langgraph configuration

---
+++ implementations/plank-agent-chat-app/apps/agents/src/day-2/command/to-parent-graph/graph.ts
@@ -0,0 +1,67 @@
+import { Annotation, Command, START, StateGraph } from "@langchain/langgraph";
+
+// Define graph state
+const StateAnnotation = Annotation.Root({
+  foo: Annotation<string>,
+});
+
+// Define the nodes
+const nodeA = async (_state: typeof StateAnnotation.State) => {
+  console.log("Called A");
+  // this is a replacement for a real conditional edge function
+  const goto = Math.random() > .5 ? "nodeB" : "nodeC";
+  // note how Command allows you to BOTH update the graph state AND route to the next node
+  return new Command({
+    // this is the state update
+    update: {
+      foo: "a",
+    },
+    // this is a replacement for an edge
+    goto,
+  });
+};
+
+// Nodes B and C are unchanged
+const nodeB = async (state: typeof StateAnnotation.State) => {
+  console.log("Called B");
+  return {
+    foo: state.foo + "|b",
+  };
+}
+
+const nodeC = async (state: typeof StateAnnotation.State) => {
+  console.log("Called C");
+  return {
+    foo: state.foo + "|c",
+  };
+}
+
+const nodeASubgraph = async (_state: typeof StateAnnotation.State) => {
+  console.log("Called A");
+  // this is a replacement for a real conditional edge function
+  const goto = Math.random() > .5 ? "nodeB" : "nodeC";
+  // note how Command allows you to BOTH update the graph state AND route to the next node
+  return new Command({
+    update: {
+      foo: "a",
+    },
+    goto,
+    // this tells LangGraph to navigate to node_b or node_c in the parent graph
+    // NOTE: this will navigate to the closest parent graph relative to the subgraph
+    graph: Command.PARENT,
+  });
+};
+
+const subgraph = new StateGraph(StateAnnotation)
+  .addNode("nodeA", nodeASubgraph)
+  .addEdge("__start__", "nodeA")
+  .compile();
+
+const parentGraph= new StateGraph(StateAnnotation)
+  .addNode("subgraph", subgraph, { ends: ["nodeB", "nodeC"] })
+  .addNode("nodeB", nodeB)
+  .addNode("nodeC", nodeC)
+  .addEdge("__start__", "subgraph")
+  .compile();
+
+export { parentGraph as graph };
