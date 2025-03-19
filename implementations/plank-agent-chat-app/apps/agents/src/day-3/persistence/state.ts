05d3ae26a1e56c0655481ae173ef94fb1d0dc833
Artur H. de Oliveira <artur.oliveira@joinplank.com>
2025-03-19 10:57:25 -0300

    day-3_persistence

---
+++ implementations/plank-agent-chat-app/apps/agents/src/day-3/persistence/state.ts
@@ -0,0 +1,10 @@
+import { Annotation } from "@langchain/langgraph";
+import { BaseMessage } from "@langchain/core/messages";
+
+const GraphState = Annotation.Root({
+  messages: Annotation<BaseMessage[]>({
+    reducer: (x, y) => x.concat(y),
+  }),
+});
+
+export { GraphState };
