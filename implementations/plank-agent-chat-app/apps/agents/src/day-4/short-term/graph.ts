77dcbff93732cec60a2afa9a9a7a48802812bb3f
Artur H. de Oliveira <artur.oliveira@joinplank.com>
2025-03-20 15:51:49 -0300

    changing folder name

---
+++ implementations/plank-agent-chat-app/apps/agents/src/day-4/short-term/graph.ts
@@ -0,0 +1,41 @@
+import llm from "./llm-call.js";
+import { toolNode, tools } from "./tools.js";
+import { END, START, StateGraph, MemorySaver } from "@langchain/langgraph";
+import { AIMessage } from "@langchain/core/messages";
+import { RunnableConfig } from "@langchain/core/runnables";
+import { GraphState } from "./state.js";
+
+const boundModel = llm.bindTools(tools);
+
+const memory = new MemorySaver();
+
+const routeMessage = (state: typeof GraphState.State) => {
+  const { messages } = state;
+  const lastMessage = messages[messages.length - 1] as AIMessage;
+  // If no tools are called, we can finish (respond to the user)
+  if (!lastMessage.tool_calls?.length) {
+    return END;
+  }
+  // Otherwise if there is, we continue and call the tools
+  return "tools";
+};
+
+const callModel = async (
+  state: typeof GraphState.State,
+  config?: RunnableConfig,
+) => {
+  const { messages } = state;
+  const response = await boundModel.invoke(messages, config);
+  return { messages: [response] };
+};
+
+const workflow = new StateGraph(GraphState)
+  .addNode("agent", callModel)
+  .addNode("tools", toolNode)
+  .addEdge(START, "agent")
+  .addConditionalEdges("agent", routeMessage)
+  .addEdge("tools", "agent");
+
+const graph = workflow.compile({ checkpointer: memory });
+
+export { graph };
